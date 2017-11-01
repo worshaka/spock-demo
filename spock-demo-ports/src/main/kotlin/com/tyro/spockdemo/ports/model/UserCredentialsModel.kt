3a8f0f6d1507ac7929c1b17ca013660e7025e7c3
Travis Jones <travis.jones@internode.on.net>
2017-11-01 12:02:53 +1100

    refactor project structure

--- spock-demo-ports/src/main/kotlin/com/tyro/spockdemo/ports/model/UserCredentialsModel.kt
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2003 - 2017 Tyro Payments Limited.
- * Lv1, 155 Clarence St, Sydney NSW 2000.
- * All rights reserved.
- */
-package com.tyro.spockdemo.ports.model
-
-class UserCredentialsModel(val username: String, val plainTextPassword: String)
